55312612b379016ab82dbc500b2bb7933cd3591f
Thabo <thabo.radebe@live.com>
2023-07-12 19:52:56 +0200

    contact and social icon

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,13 +1,25 @@
-import {AiOutlineInstagram} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
-import {AiFillGithub} from 'react-icons/ai'
+import { FaLinkedin } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
 
 const data = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
-]
+  {
+    id: 1,
+    link: "https://www.linkedin.com/in/thabo-radebe-35860a29",
+    icon: <FaLinkedin />,
+  },
+  { id: 2, link: "https://twitter.com", icon: <AiOutlineTwitter /> },
+  {
+    id: 3,
+    link: "https://www.facebook.com/RadebeThaboHenrdy?mibextid=ZbWKwL",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 4,
+    link: " https://github.com/Stiboo2?tab=repositories",
+    icon: <AiFillGithub />,
+  },
+];
 
-export default data
+export default data;
